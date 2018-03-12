dcf5e38e3b144b982f1b1d902a3eb93e24353fed
Bwarhness <nalar94@gmail.com>
2018-03-12 01:06:30 +0100

    pls

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -8,8 +8,8 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class ApiProvider {
-// Api = "http://fitnessapi.webdesk-dev.dk";
-Api = "http://localhost:50590";
+Api = "http://fitnessapi.webdesk-dev.dk";
+// Api = "http://localhost:50590";
 
 
 Token:any;
